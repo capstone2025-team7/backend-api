1fecc165f9c40654bb18dc6a91345ec222aa90ab
JinHyeongSeo <jh4clover@naver.com>
2025-07-16 22:34:24 +0900

    init: Spring Boot 초기 세팅

---
+++ src/main/java/com/capstone2025/team7/backend/BackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.capstone2025.team7.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone2025/team7/backend/BackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone2025.team7.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
