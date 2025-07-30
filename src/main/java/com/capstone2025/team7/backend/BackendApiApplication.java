68f616f062c455bb59fc73a0bc2f401cba9fd6be
kimjunha <rlawnsgk0202@hs.ac.kr>
2025-07-30 17:21:48 +0900

    EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/capstone2025/team7/backend/BackendApiApplication.java
+++ src/main/java/com/capstone2025/team7/backend/BackendApiApplication.java
@@ -2,7 +2,9 @@ package com.capstone2025.team7.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BackendApiApplication {
 
