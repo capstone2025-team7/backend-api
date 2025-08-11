49f3910b04b57ce82ad862d809f021d948276b7a
kimjunha <rlawnsgk0202@hs.ac.kr>
2025-08-11 17:46:48 +0900

    vote 패키지 생성

---
+++ src/main/java/com/capstone2025/team7/backend/vote/entity/Vote.java
@@ -0,0 +1,29 @@
+package com.capstone2025.team7.backend.vote.entity;
+
+import com.capstone2025.team7.backend.auditable.Auditable;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Vote extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vote_id", nullable = false)
+    private long voteId;
+
+    @Column(name = "title", length = 50, nullable = false)
+    private String title;
+
+    @Column(name = "descriptions", length = 50, nullable = false)
+    private String descriptions;
+
+}
---
+++ src/main/java/com/capstone2025/team7/backend/vote/service/VoteService.java
@@ -0,0 +1,4 @@
+package com.capstone2025.team7.backend.vote.service;
+
+public class VoteService {
+}
